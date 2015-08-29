e9f35c440abcea30ac928fec32059d1394463238
kennygeiler <kennethlgeiler@gmail.com>
2015-08-29 14:26:08 -0500

    delete teachers

--- tutor/config/routes.rb
+++ tutor/config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
     resources :courses, only: [:index, :show]
   end
 
+  resources :teachers
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
