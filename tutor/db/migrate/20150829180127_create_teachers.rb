8db5ca988f1362f4ee9622ca81fcf26722cee037
kennygeiler <kennethlgeiler@gmail.com>
2015-08-29 13:05:35 -0500

    migrations completed

--- tutor/db/migrate/20150829180127_create_teachers.rb
+++ tutor/db/migrate/20150829180127_create_teachers.rb
@@ -1,7 +1,9 @@
 class CreateTeachers < ActiveRecord::Migration
   def change
     create_table :teachers do |t|
-
+      t.string :name
+      t.string :email
+      t.references :school
       t.timestamps null: false
     end
   end
